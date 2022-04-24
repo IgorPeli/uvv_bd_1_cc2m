b48d3a219493069e8f2f65126cc01cf46679923a
igorpeli <103079100+IgorPeli@users.noreply.github.com>
2022-04-24 14:42:52 -0300

    Update inset de dados-terceiro.sql

--- pset1/MariaDB-MySQL/inset de dados-terceiro.sql
+++ pset1/MariaDB-MySQL/inset de dados-terceiro.sql
@@ -1,27 +1,26 @@
 -- INSERÇÃO DE DADOS NA TABELA FUNCIONARIO 
 
-
 INSERT INTO uvv.funcionario (primeiro_nome, nome_meio, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
  
-VALUES ('Jorge', 'E', 'Brito', '88866555576', 1937/11/10, 'Rua do Horto, 35, São Paulo', 'M', 55000, NULL, 1);
+VALUES ('Jorge', 'E', 'Brito', '88866555576', '1937-11-10', 'Rua do Horto, 35, São Paulo', 'M', 55000, NULL, 1);
 
 INSERT INTO uvv.funcionario
  (primeiro_nome, nome_meio, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
  
  VALUES
- ('Fernando', 'T', 'Wong', '33344555587', 1955/12/08, 'Rua da Lapa, 34, São Paulo, SP', 'M', 40000, '88866555576', 1),
- ('Jennifer', 'S', 'Souza', '98765432168', 1941/06/20, 'Av. Arthur de Lima, 54, Santo Andre, SP', 'F', 43000, '88866555576', 1);
+ ('Fernando', 'T', 'Wong', '33344555587', '1955-12-08', 'Rua da Lapa, 34, São Paulo, SP', 'M', 40000, '88866555576', 1),
+ ('Jennifer', 'S', 'Souza', '98765432168', '1941-06-20', 'Av. Arthur de Lima, 54, Santo Andre, SP', 'F', 43000, '88866555576', 1);
  
  
  INSERT INTO uvv.funcionario
 (primeiro_nome, nome_meio, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento) 
 
 VALUES
- ('João', 'B', 'Silva', '12345678966', 1965/01/09, 'Rua das Flores, 751, São Paulo, SP', 'M', 30000, '33344555587', 5),
- ('Alice', 'J', 'Zelaya', '99988777767', 1968/01/19, 'Rua Souza Lima, 35, Curitiba, PR', 'F', 25000, '98765432168', 4),
- ('Ronaldo', 'K', 'Lima', '66688444476', 1962/09/15, 'Rua Reboucas, 65, Piracicaba, SP', 'M', 38000, '33344555587', 5),
- ('Joice', 'A', 'Leite', '45345345376', 1972/07/31, 'Av. Lucas Obes, 74, São Paulo, SP','F', 25000, '33344555587', 5),
- ('André', 'V', 'Pereira', '98798798733', 1969/03/29, 'Rua Timbira, 35, São Paulo, SP', 'M', 25000, '98765432168', 4);
+ ('João', 'B', 'Silva', '12345678966', '1965-01-09', 'Rua das Flores, 751, São Paulo, SP', 'M', 30000, '33344555587', 5),
+ ('Alice', 'J', 'Zelaya', '99988777767', '1968-01-19', 'Rua Souza Lima, 35, Curitiba, PR', 'F', 25000, '98765432168', 4),
+ ('Ronaldo', 'K', 'Lima', '66688444476', '1962-09-15', 'Rua Reboucas, 65, Piracicaba, SP', 'M', 38000, '33344555587', 5),
+ ('Joice', 'A', 'Leite', '45345345376', '1972-07-31', 'Av. Lucas Obes, 74, São Paulo, SP','F', 25000, '33344555587', 5),
+ ('André', 'V', 'Pereira', '98798798733', '1969-03-29', 'Rua Timbira, 35, São Paulo, SP', 'M', 25000, '98765432168', 4);
  
 
  
@@ -31,9 +30,9 @@ VALUES
  (nome_departamento, numero_departamento, cpf_gerente, data_inicio_gerente)
  
  VALUES
- ('Matriz', 1, '88866555576', 1981/06/19),
- ('Pesquisa', 4, '33344555587', 1988/05/22),
- ('Administração', 5, '98765432168', 1995/01/01);
+ ('Matriz', 1, '88866555576', '1981-06-19'),
+ ('Pesquisa', 4, '33344555587', '1988-05-22'),
+ ('Administração', 5, '98765432168', '1995-01-01');
  
  -- INSERÇÃO DE DADOS NA TABELA PROJETO 
 
@@ -93,11 +92,11 @@ insert INTO uvv.localizacoes_departamento
     (cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
     
     VALUES
- ('33344555587', 'Alicia', 'F', 1986/04/05, 'Filha'),
- ('33344555587', 'Tiago', 'M', 1983/10/25, 'Filho'),
- ('33344555587', 'Janaina', 'F', 1958/05/03, 'Esposa'),
- ('98765432168', 'Antonio', 'M', 1942/02/28, 'Marido'),
- ('12345678966', 'Michael', 'M', 1988/01/04, 'Filho'),
- ('12345678966', 'Alicia', 'F', 1988/12/30, 'Filha'),
- ('12345678966', 'Elizabeth', 'F', 1967/05/05, 'Esposa');
+ ('33344555587', 'Alicia', 'F', '1986-04-05', 'Filha'),
+ ('33344555587', 'Tiago', 'M', '1983-10-25', 'Filho'),
+ ('33344555587', 'Janaina', 'F', '1958-05-03', 'Esposa'),
+ ('98765432168', 'Antonio', 'M', '1942-02-28', 'Marido'),
+ ('12345678966', 'Michael', 'M', '1988-01-04', 'Filho'),
+ ('12345678966', 'Alicia', 'F', '1988-12-30', 'Filha'),
+ ('12345678966', 'Elizabeth', 'F', '1967-05-05', 'Esposa');
  -- Fim da inserção de dados
